8339a2ce83ce2e331689cc294d754361920ffec7
Fran Alvarez <fjalvarez@gmail.com>
2018-03-18 19:51:11 +0100

    Preauthorize methods

--- src/main/java/com/fjalvarez/review/ReviewRepository.java
+++ src/main/java/com/fjalvarez/review/ReviewRepository.java
@@ -2,6 +2,16 @@ package com.fjalvarez.review;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface ReviewRepository extends PagingAndSortingRepository<Review, Long> {
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN') OR @reviewRepository.findOne(#id)?.reviewer?.username == authentication.name")
+    void delete(@Param("id") Long aLong);
+
+    @Override
+    @PreAuthorize("hasRole('ROLE_ADMIN') OR #review.revierer?.username == authentication.name")
+    void delete(@Param("review") Review entity);
 }
